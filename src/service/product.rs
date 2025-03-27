2d5d58f1f989bf8e98164dcce927aa57b283da88
Brian <briansubekti11@gmail.com>
2025-03-27 23:38:20 +0700

    Implement publish function in Program service and Program controller.

--- src/service/product.rs
+++ src/service/product.rs
@@ -5,41 +5,57 @@ use bambangshop::{Result, compose_error_response};
 use crate::model::product::Product;
 use crate::repository::product::ProductRepository;
 
+use super::notification::NotificationService;
+
 pub struct ProductService;
-
-impl ProductService {
-    pub fn create(mut product: Product) -> Result<Product> {
-        product.product_type = product.product_type.to_uppercase();
-        let product_result: Product = ProductRepository::add(product);
-
-        return Ok(product_result);
-    }
-
-    pub fn list() -> Result<Vec<Product>> {
-        return Ok(ProductRepository::list_all());
-    }
-
-    pub fn read(id: usize) -> Result<Product> {
-        let product_opt: Option<Product> = ProductRepository::get_by_id(id);
-        if product_opt.is_none() {
-            return Err(compose_error_response(
-                Status::NotFound,
-                String::from("Product not found.")
-            ));
-        }
-        return Ok(product_opt.unwrap());
-    }
-
-    pub fn delete(id: usize) -> Result<Json<Product>> {
-        let product_opt: Option<Product> = ProductRepository::delete(id);
-        if product_opt.is_none() {
-            return Err(compose_error_response(
-                Status::NotFound,
-                String::from("Product not found.")
-            ));
-        }
-        let product: Product = product_opt.unwrap();
-
-        return Ok(Json::from(product));
-    }
-}
+ 
+ impl ProductService {
+     pub fn create(mut product: Product) -> Result<Product> {
+         product.product_type = product.product_type.to_uppercase();
+         let product_result: Product = ProductRepository::add(product);
+ 
+         return Ok(product_result);
+     }
+ 
+     pub fn list() -> Result<Vec<Product>> {
+         return Ok(ProductRepository::list_all());
+     }
+ 
+     pub fn read(id: usize) -> Result<Product> {
+         let product_opt: Option<Product> = ProductRepository::get_by_id(id);
+         if product_opt.is_none() {
+             return Err(compose_error_response(
+                 Status::NotFound,
+                 String::from("Product not found.")
+             ));
+         }
+         return Ok(product_opt.unwrap());
+     }
+ 
+     pub fn delete(id: usize) -> Result<Json<Product>> {
+         let product_opt: Option<Product> = ProductRepository::delete(id);
+         if product_opt.is_none() {
+             return Err(compose_error_response(
+                 Status::NotFound,
+                 String::from("Product not found.")
+             ));
+         }
+         let product: Product = product_opt.unwrap();
+ 
+         return Ok(Json::from(product));
+     }
+ 
+     pub fn publish(id: usize) -> Result<Product> {
+         let product_opt: Option<Product> = ProductRepository::get_by_id(id);
+         if product_opt.is_none() {
+             return Err(compose_error_response(
+                 Status::NotFound,
+                 String::from("Product not found.")
+             ));
+         }
+         let product: Product = product_opt.unwrap();
+     
+         NotificationService.notify(&product.product_type, "PROMOTION", product.clone());
+         return Ok(product);
+     }    
+ }
